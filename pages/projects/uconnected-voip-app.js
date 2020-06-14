16a78a184a1e8ac90bf79682b316672fb030a1e8
Madhava Di Florio <mattdiflorio@gmail.com>
2020-06-14 17:19:39 +0200

    Add uConnected descriptions

--- pages/projects/uconnected-voip-app.js
+++ pages/projects/uconnected-voip-app.js
@@ -35,23 +35,47 @@ export default function UconnectedVoipApp() {
                             showPlayButton={false}
                         />
                     </div>
+                    <h2>Context</h2>
                     <p>
-                        An IOS and Android application built using React native
-                        for uConnected. The app allows the clients of uConnected
-                        to connect and make calls through their VoIP service and
-                        includes various features such as user authentication,
-                        access to the device's contacts list complete with an
-                        alphabetical scroll list, a keypad, and an integration
-                        of uConnected's dashboard.\n\nThe app incorporates a
-                        variety of libraries such as Redux, React Native
-                        Navigation and React Native PJSIP. It contains a range
-                        of animations to ensure the user experience is smooth
-                        and in line with a native user experience.\n\nYou can
-                        visit uConnected's website{" "}
-                        <a target="_blank" href="https://www.uconnected.com.au">
-                            here
-                        </a>
-                        .
+                        uConnected provides a service that allows you to choose
+                        a landline, 1800 or 1300 number and you can decide where
+                        calls to those numbers are delivered. uConnected
+                        provides an amazing range of options such as the ability
+                        to switch your selected number at any time, limiting
+                        hours that you receive calls and multiple redirections.
+                        This gives them a competitive advantage over other large
+                        companies that may provide similar services.
+                    </p>
+                    <h2>Problem</h2>
+                    <p>
+                        While uConnected has many incredible features, their
+                        customers are unable to make outbound calls using their
+                        service.
+                    </p>
+                    <h2>Solution</h2>
+                    <p>
+                        A solution to this problem was to develop a VoIP
+                        application that allowed you to make outbound calls
+                        through the service. The basis of this application was
+                        on a technology called PJSIP which is an open-source SIP
+                        protocol. The application needed to be functional on
+                        both Android and IOS.
+                    </p>
+                    <h2>Note</h2>
+                    <p>
+                        Even though the application was finished and worked as
+                        intended. The team at uConnected was not satisfied with
+                        the reliability of the PJSIP. Consequently, the
+                        application was never pushed into production.
+                    </p>
+                    <h2>Technologies</h2>
+                    <p>
+                        <span className="colored-text">Frontend: </span>
+                        React Native, Redux, React-Native-PJSIP
+                    </p>
+                    <p>
+                        <span className="colored-text">Platforms: </span>
+                        IOS, Android
                     </p>
                 </div>
             </div>
